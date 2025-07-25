522078f73e817fe335b86f00a29376df4bfe4711
bdgeraghty <bdgeraghty@gmail.com>
2025-07-24 23:14:35 -0400

    AIAgent End of Chapter 1

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+# Check if the script is run with a command line argument 
+if len(sys.argv) < 2:
+    print("Usage: python main.py <prompt>")
+    sys.exit(1)
+else:
+    user_prompt = sys.argv[1]
+
+verbose = False
+if len(sys.argv) == 3:
+    if sys.argv[2] == "--verbose":
+        verbose = True
+
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages,
+)
+
+#print(response.text)
+if verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
