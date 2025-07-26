11dba2dd575f8d28a9b2cdd1faaee1285f5c8377
bdgeraghty <bdgeraghty@gmail.com>
2025-07-25 21:47:18 -0400

    schema is added for each call - working point

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,6 @@
 import os
 import config as cfg
+from google.genai import types
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
@@ -19,3 +20,19 @@ def get_file_content(working_directory, file_path):
         return f"Error reading file: {e}"
 
     return content[:cfg.MAX_CHARS]
+
+
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Read file contents from the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The file to read from, relative to the working directory. If not provided, reads from the working directory itself.",
+            ),
+        },
+    ),
+)
